d123cdc9eab05522e8bc7a100b959efee7de0bc4
colynn.liu <350851839@qq.com>
2019-09-06 20:04:11 +0800

    add health interface

--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,15 @@ package routers
 
 import (
 	"hanker-hello/controllers"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+
+	beego.Get("/health", func(ctx *context.Context) {
+		ctx.Output.Body([]byte("Ok"))
+	})
 }
